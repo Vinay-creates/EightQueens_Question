a2b55355b0bfcc9c77395f5a31f5396e59ed1577
RAMA VINAY <RAMA VINAY@LAPTOP-FR3UV9ET>
2020-07-09 18:52:33 +0530

    Working Chess board game

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,17 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		boolean color = true;
+		if((square/8) % 2 == 0) {
+			if(square % 2 == 0) {
+				color = false;
+			}
+		}
+		else {
+			if(square % 2 == 1)
+				color = false;
+		}
+		
+		return color;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -3,20 +3,22 @@ public class GameStrategy {
 	private int numQueens = 0;
 
 	private int getColumn(int cellId) {
-		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		// WRITE YOUR LOGIC HERE...................................	
+		
+		int cn = cellId%8;
+		return cn;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
 		
-		return 0;	// just for the heck of it
+		int rn = cellId/8;
+		return rn;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
 		boolean isValid = true;
-
+        
 		if(numQueens == 8) throw new GameOverException();	// just return out of the method
 
 		// the game is still on, so let us continue...
@@ -24,11 +26,66 @@ public class GameStrategy {
 		int col = getColumn(cellId);
 		
 		int row = getRow(cellId);
-		
 		/*
 			WRITE YOUR LOGIC HERE...............................
 
 		*/
+//		[row][col]
+		for(int i =0;i < 8;i++) {
+			if(placedQueens[i][col] == true) {
+				if(i != row) {
+					return false;
+				}
+			}
+		}
+		for(int i =0;i < 8;i++) {
+			if(placedQueens[row][i] == true) {
+				if(i != col) {
+					return false;
+				}
+			}
+		}
+		int row1= row;
+		int col1= col;
+		while(row1 < 8 && col1 < 8 && row1>=0 && col1>=0) {
+			if(placedQueens[row1][col1] == true) {
+				return false;
+			}
+			row1++;
+			col1++;
+		}
+		
+	    row1= row;
+		col1= col;
+		while(row1 < 8 && col1 < 8 && row1>=0 && col1>=0) {
+			if(placedQueens[row1][col1] == true) {
+				return false;
+			}
+			row1++;
+			col1--;
+		}
+		
+		row1= row;
+		col1= col;
+		while(row1 < 8 && col1 < 8 && row1>=0 && col1>=0) {
+			if(placedQueens[row1][col1] == true) {
+				return false;
+			}
+			row1--;
+			col1--;
+		}
+		
+		row1= row;
+		col1= col;
+		while(row1 < 8 && col1 < 8 && row1>=0 && col1>=0) {
+			if(placedQueens[row1][col1] == true) {
+				return false;
+			}
+			row1--;
+			col1++;
+		}
+		placedQueens[row][col] = true;	
+		numQueens++;
 		return isValid;
 	}
 }
